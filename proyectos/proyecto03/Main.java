17b698c966052937f474d31a2b22dbc7cf012fc1
GabrieCosson <cosson.m.gabriel@ciencias.unam.mx>
2025-05-14 13:40:16 -0600

    Add files via upload

---
+++ proyectos/proyecto03/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public class Carta {
+        private int feliz;  
+        private int lentes;  
+        private int sombrero;  
+        private int alto;  
+        private int attribute5;  
+    
+  
+        public Carta(int a1, int a2, int a3, int a4, int a5) {
+            this.feliz = a1;
+            this.lentes = a2;
+            this.sombrero = a3;
+            this.alto = a4;
+            this.cejas = a5;
+        }
+    
+
+        public int getfeliz() { return feliz; }
+        public int getlentes() { return lentes; }
+        public int getsombrero() { return sombrero; }
+        public int getalto() { return alto; }
+        public int getcejas() { return cejas; }
+    
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
