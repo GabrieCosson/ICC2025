c1900b11b2effa6ef28dc43440dac90b87dcb03a
Maxi-rg <lsdmax2@gmail.com>
2025-06-02 05:23:56 -0600

    Update Carta.java

--- proyectos/proyecto03/Carta.java
+++ proyectos/proyecto03/Carta.java
@@ -1,28 +1,37 @@
+import java.io.*;
 public class Carta {
-    private int feliz;  
+    private int sueter;  
     private int lentes;  
     private int sombrero;  
-    private int alto;  
-    private int cejas;  
-
-
-    public Carta(int a1, int a2, int a3, int a4, int a5) {
-        this.feliz = a1;
-        this.lentes = a2;
-        this.sombrero = a3;
-        this.alto = a4;
-        this.cejas = a5;
+    private int guantes;  
+    private int bufanda;  
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m"; //rojo
+    public static final String ANSI_GREEN = "\u001B[32m"; //verde
+    public static final String ANSI_YELLOW = "\u001B[33m"; //amarillo
+    public static final String ANSI_BLUE = "\u001B[34m"; //azul
+    public static final String ANSI_CYAN = "\u001B[36m"; //cyan
+    public Carta(int sueter, int lentes, int sombrero, int guantes, int bufanda) {
+        this.sueter = sueter;
+        this.lentes = lentes;
+        this.sombrero = sombrero;
+        this.guantes = guantes;
+        this.bufanda = bufanda;
     }
-    @Override
     public String toString() {
-        return feliz + "" + lentes + "" + sombrero + "" + alto + "" + cejas;
+        return (sueter == 0 ? ANSI_RED + "sueter" + ANSI_RESET : ANSI_BLUE + "sueter" + ANSI_RESET) + 
+               (lentes == 0 ? ANSI_GREEN + "lentes" + ANSI_RESET : ANSI_CYAN + "lentes" + ANSI_RESET) + 
+               (sombrero == 0 ? ANSI_RED + "sombrero" + ANSI_RESET : ANSI_YELLOW + "sombrero" + ANSI_RESET) + 
+               (guantes == 0 ? ANSI_CYAN + "guantes" + ANSI_RESET : ANSI_GREEN + "guantes" + ANSI_RESET) + 
+               (bufanda == 0 ? ANSI_YELLOW + "bufanda" + ANSI_RESET : ANSI_BLUE + "bufanda" + ANSI_RESET);
     }
-    
-    public int getfeliz() { return feliz; }
-    public int getlentes() { return lentes; }
-    public int getsombrero() { return sombrero; }
-    public int getalto() { return alto; }
-    public int getcejas() { return cejas; }
-
-
+    public int diferenciasCon(Carta otra) {
+        int dif = 0;
+        if (this.sueter != otra.sueter) dif++;
+        if (this.lentes != otra.lentes) dif++;
+        if (this.sombrero != otra.sombrero) dif++;
+        if (this.guantes != otra.guantes) dif++;
+        if (this.bufanda != otra.bufanda) dif++;
+        return dif;
+    }
 }
