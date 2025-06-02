b5c3cb01c3d001d7986b1b02ffa058b2782178e3
Maxi-rg <lsdmax2@gmail.com>
2025-06-02 05:25:28 -0600

    Update ListaLigada.java

--- proyectos/proyecto03/ListaLigada.java
+++ proyectos/proyecto03/ListaLigada.java
@@ -1,67 +1,39 @@
 public class ListaLigada{
     class Nodo{
-        private Object elemento;
+        private Carta elemento;
         private Nodo siguiente;
-        //referencia al nodo siguiente
-        public Nodo(Object elemento){
+        public Nodo(Carta elemento){
             this.elemento = elemento;
         }
     }
-    Nodo cabeza;
-    static int tamaño = 0;
-    public ListaLigada(){
+    public Nodo cabeza;
+    static int tamaño;
+    public ListaLigada() {
         cabeza = null;
         tamaño = 0;
-        //lista vacia y variable de clase que indica el tamaño
     }
-    public void insertarElemento(int elemento){
-        Nodo nuevo = new Nodo (elemento);
-        if (cabeza == null){
-            cabeza = nuevo;
-           tamaño++;
+    public void agregarCarta(Carta carta){
+        Nodo agregado = new Nodo(carta);
+        if(cabeza ==null){
+            cabeza = agregado;
         }else{
-            Nodo ubicacion = cabeza;
+           Nodo ubicacion = cabeza;
             while(ubicacion.siguiente != null){
                 ubicacion = ubicacion.siguiente;
             }
-            ubicacion.siguiente = nuevo;
+            ubicacion.siguiente = agregado;
             tamaño++;
-        }
-        //metodo insertar siempre ubicandose al final el nuevo y agregando valor a la referencia del tamaño
+        }  
     }
-    public void eliminarElemento (int elemento){
-        if(cabeza == null){
-            return;
+    public Nodo obtenerUltima() {
+        if (cabeza == null) return null;
+        Nodo actual = cabeza;
+        while (actual.siguiente != null) {
+            actual = actual.siguiente;
         }
-        //lista vacia
-        if(cabeza.elemento == elemento){
-            cabeza = cabeza.siguiente;
-            tamaño--;
-            return;
-        }
-        //lista con un solo elemento o eliminar el primero
-        Nodo ubicacion = cabeza;
-        while(ubicacion.siguiente != null && ubicacion.siguiente.elemento != elemento){
-            ubicacion = ubicacion.siguiente;
-        }
-        if(ubicacion.siguiente != null){
-            ubicacion.siguiente = ubicacion.siguiente.siguiente;
-            tamaño--;
-        }
-        //Se elimina el dato ubicandolo con la referencia y aseguramos que no sea vacio para evitar NullPointerException
+        return actual;
     }
-    public String toString(){
-        if(cabeza == null){
-            return "La lista esta vacía.";
-        }else{
-            Nodo ubicacion = cabeza;
-            String visual = Integer.toString(ubicacion.elemento);
-            while(ubicacion.siguiente != null){
-                visual += "->" + Integer.toString(ubicacion.siguiente.elemento);
-                ubicacion = ubicacion.siguiente;
-            }
-            visual += "-> null";
-            return visual;
-        }
+    public int tamaño() {
+        return tamaño;
     }
 }
